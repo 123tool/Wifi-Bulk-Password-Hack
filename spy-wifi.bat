b5b608b460f020c29d6d75d015541431fe28c874
𝗦𝗣𝗬-𝗘 <161654922+123tool@users.noreply.github.com>
2026-04-03 09:48:05 +0700

    Create spy-wifi.bat

---
+++ spy-wifi.bat
@@ -0,0 +1,26 @@
+@echo off
+title SPY-WIFI-EXTRACTOR [123Tool]
+color 0b
+echo ==========================================
+echo    SPY-WIFI-EXTRACTOR
+echo    Coded by SPY-E & 123Tool
+echo ==========================================
+echo.
+echo [*] Sedang menarik data password...
+echo ------------------------------------------
+
+setlocal enabledelayedexpansion
+
+for /f "tokens=2 delims=:" %%a in ('netsh wlan show profiles ^| findstr "All User Profile"') do (
+    set ssid=%%a
+    set ssid=!ssid:~1!
+    for /f "tokens=2 delims=:" %%b in ('netsh wlan show profile name^="!ssid!" key^=clear ^| findstr "Key Content"') do (
+        set pass=%%b
+        echo SSID: !ssid! ^| Password: !pass:~1!
+    )
+)
+
+echo.
+echo ------------------------------------------
+echo [+] Ekstraksi Selesai, Bosku!
+pause
